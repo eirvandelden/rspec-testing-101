90e2fff02a291c8fdf6321ae7f4306b1d13700fc
Etienne van Delden de la Haije <etienne@vandelden.family>
2025-05-19 16:34:18 +0200

    add first tests to showcase rspec

--- spec/tutorial/1-minimal_test_spec.rb
+++ spec/tutorial/1-minimal_test_spec.rb
@@ -1,7 +1,10 @@
 require 'rails_helper'
 
+# Docs are here: rspec.info
+# You "describe" the thing you want to test.
+# "it" must conform to several behaviours.
 RSpec.describe PlayerCharacter, type: :model do
-  it "has a name" do
+  it "can create a Deekin" do
     deekin = PlayerCharacter.new(name: "Deekin Scalesinger")
     expect(deekin.name).to eq "Deekin Scalesinger"
   end
